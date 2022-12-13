b5109015f89509c8e175f325bcb6f6ba6506965c
Acr0l <>
2022-12-12 22:30:14 -0300

    Adding Classes

---
+++ constants/locations.js
@@ -0,0 +1,41 @@
+const BaseClass = require("./BaseClass.js");
+const Materials = require("./materials.js");
+
+/** Class representing a location 
+ * @extends BaseClass
+*/
+class Location extends BaseClass {
+  /**
+   * Create a location
+   * @param {String} displayName - The name for the user
+   * @param {String} name - The name for system and back-end
+   * @param {Number} rank - Value to define probabilities and more.
+   * @param {Number} lvlReq - The lvl of the user to travel to the location.
+   */
+  constructor(displayName, name, rank, lvlReq = 0) {
+    super(displayName, name);
+    this.rank = rank;
+    this.materials = [];
+    this.lvlReq = lvlReq
+  }
+  get lMaterials() {
+    return this.materials;
+  }
+  set lMaterials(materials) {
+    const rankLeveler = Math.pow(Math.E, this.rank - 1) - 1;
+    const total =
+      materials.reduce((a, b) => a.rarity + b.rarity) +
+      rankLeveler * materials.length;
+    this.materials = materials.map((material) => ({
+      ...material,
+      rarity: (total - material.rarity - rankLeveler) / total,
+    }));
+  }
+}
+
+const village = new Location("Village", "village", 0);
+const smallMountains = new Location("Small Mountains", "small-mountains", 1);
+smallMountains.lMaterials = Materials.filter(e => e.locations.includes(smallMountains.name));
+
+
+module.exports = { [village.name]: village, [smallMountains.name]: smallMountains };
