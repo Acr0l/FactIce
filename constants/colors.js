e2558751506431a844950b2eef41ed3b6012d31b
Acr0l <>
2022-12-18 08:57:10 -0300

    ADDED Constants - Colors

---
+++ constants/colors.js
@@ -0,0 +1,14 @@
+module.exports = {
+  /** @type {import('discord.js').ColorResolvable } */
+  PRIMARY: "#03045E",
+  /** @type {import('discord.js').ColorResolvable } */
+  SECONDARY: "#023E8A",
+  /** @type {import('discord.js').ColorResolvable } */
+  TERTIARY: "#0096C7",
+  /** @type {import('discord.js').ColorResolvable } */
+  QUATERNARY: "#48CAE4",
+  /** @type {import('discord.js').ColorResolvable } */
+  QUINARY: "#CAF0F8",
+  /** @type {import('discord.js').ColorResolvable } */
+  FAILURE: "#f72585",
+};
